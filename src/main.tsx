684795573dc8820c97dfc0e8173962a24fc08d02
JAI <jaiprakash@hasteck.onmicrosoft.com>
2023-09-30 22:22:29 +0530

    Added: HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route, HashRouter } from 'react-router-dom'
 import { HouseDetail } from './component/HouseDetail/HouseDetail.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router>
+    <HashRouter>
       {
         <div style={{ width: '100%', height: '20vh', backgroundColor: '#002451', marginBottom: '20px', display: 'flex', justifyContent: 'center', color: 'white', alignItems: 'center', fontSize: '30px' }}>
           DirectLender.com
@@ -16,6 +16,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <Route path='*' element={<App />} />
         <Route path='/detail' element={<HouseDetail />} />
       </Routes>
-    </Router>
+    </HashRouter>
   </React.StrictMode>,
 )
